27e2d9895b1790d252e6ec64e1eb503f0d32a81d
sapanagupta25 <146055257+sapanagupta25@users.noreply.github.com>
2025-03-26 11:09:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,15 +20,5 @@ class SportsCar extends Car {
     }
 }
 
-module.exports = { Car, SportsCar };
 
-
-const { Car, SportsCar } = require('../path/to/script');
-
-describe('Car and SportsCar classes', () => {
-    it('should create a new SportsCar instance', () => {
-        const car = new SportsCar("Ferrari", "Testarossa", 200);
-        expect(car).to.be.an.instanceof(Car);
-        // your other tests here
-    });
 });
